98b232f0ff31512f6d713ff45ae99d999ce9af52
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2021-02-07 00:14:24 +0900

    upload ch3 fig

--- R_Code/ch3.R
+++ R_Code/ch3.R
@@ -1,7 +1,7 @@
 
 # Chapter 3 マルコフ連鎖モンテカルロ法の一般論 ---------------------------------------------
 
-# 利用パッケージ
+# 3章で利用するパッケージ
 library(tidyverse)
 
 
@@ -23,11 +23,11 @@ res_df <- tibble() # 初期化
 for(s in 1:n_simu) {
   # ランダムウォーク
   tmp_df <- tibble(
-    x = sample(x = c(1, -1), size = K, replace = TRUE, prob = c(p, 1 - p)), 
     k = 1:K, # 繰り返し番号
+    x = sample(x = c(1, -1), size = K, replace = TRUE, prob = c(p, 1 - p)), # ステップ幅を生成
+    y = cumsum(x), # k番目までの合計
     simulation = as.factor(s) # シミュレーション番号
-  ) %>% 
-    dplyr::mutate(y = cumsum(x)) # k番目までの合計
+  )
   
   # 結果を結合
   res_df <- rbind(res_df, tmp_df)
@@ -47,69 +47,108 @@ ggplot() +
   geom_line(data = res_df, aes(x = k, y = y, color = simulation), 
             alpha = 0.5) + # yの推移
   geom_hline(aes(yintercept = 0), linetype = "dashed") + # 期待値
-  geom_line(data = addline_df, aes(x = k, y = root_k), linetype = "dashed") + 
-  geom_line(data = addline_df, aes(x = k, y = -root_k), linetype = "dashed") + 
+  geom_line(data = addline_df, aes(x = k, y = root_k), linetype = "dashed") + # 補助線(上)
+  geom_line(data = addline_df, aes(x = k, y = -root_k), linetype = "dashed") + # 補助線(下)
   theme(legend.position = "none") + # 凡例
   labs(title = "Random Walk", 
-       subtitle = paste0("p=", p, ", K=", k, ", simulation:", n_simu))
+       subtitle = paste0("p=", p, ", K=", K, ", simulation:", n_simu))
 
 
 # 3.2 規約性 -----------------------------------------------------------------
 
 ### ？
 
-# 定数を指定
-c <- 1
+# 確率分布を指定
+fn_P <- function(x) {
+  exp(-1 / x^2 - x^2)
+}
 
-# xの分布を確認
-f_df <- tibble(
+# 一様分布の範囲(ステップ幅)を指定
+c <- 2
+
+# 真の分布を計算
+true_df <- tibble(
   x = seq(-c, c, 0.01), 
-  y = exp(-1 / x^2 - x^2)
+  P_x = fn_P(x)
 )
-ggplot(f_df, aes(x = x, y = y)) + 
+
+# 真の分布をプロット
+ggplot(true_df, aes(x = x, y = P_x)) + 
   geom_line()
+
+
+# 作用を指定
+fn_S <- function(x) {
+  -1 / x^2 - x^2
+}
+
+# 初期値を指定
+x <- -2
 
 
 # 繰り返し回数を指定
 K <- 1000
 
-# シミュレーション回数を指定
-n_simu <- 10
-
-# 描画範囲を生成
-x <- seq(-c, c, 0.01)
-
-# 確率分布を計算
-P_x <- exp(-1 / x^2 - x^2)
-
-# シミュレーション
-res_df <- tibble() # 初期化
-for(s in 1:n_simu) {
-  # サンプルを生成
-  tmp_df <- tibble(
-    delta_x = sample(x = x, size = n_iter, replace = TRUE, prob = P_x), 
-    k = 1:K, 
-    simulation = as.factor(s)
-  ) %>% 
-    dplyr::mutate(x = cumsum(delta_x) / k) # k番目までの合計
+# Main loop
+trace_x <- rep(0, K) # 初期化
+for(k in 1:K) {
+  # xの更新候補を計算
+  dash_x <- x + runif(n = 1, min = -c, max = c)
   
-  # 結果を結合
-  res_df <- rbind(res_df, tmp_df)
+  # テスト値を生成
+  r <- runif(n = 1, min = 0, max = 1)
   
-  # 途中経過を表示
-  print(paste0("simuration: ", s, ", y = ", tmp_df[["x"]][K]))
+  # xを更新
+  if(r < exp(fn_S(x) - fn_S(dash_x))) { # メトロポリステスト
+    x <- dash_x
+  }
+  
+  # 更新値を記録
+  trace_x[k] <- x
 }
 
-# 作図
-ggplot(res_df, aes(x = k, y = x, color = simulation)) + 
-  geom_line(alpha = 0.5) + # yの推移
-  geom_hline(aes(yintercept = 0), linetype = "dashed", color = "red") + # 期待値
-  theme(legend.position = "none") + # 凡例
-  labs(title = expression(x^(k+1) == x^(k) + exp(-1 / x^2 - x^2)), 
-       subtitle = paste0("simulation: ", n_simu), 
-       x = "k")
+# 更新値のデータフレームを作成
+update_df <- tibble(
+  k = 1:K, 
+  x = trace_x
+)
+
+# ヒストグラムを作成
+ggplot() + 
+  geom_histogram(data = update_df, aes(x = x, y = ..density..), 
+                 binwidth = 0.05) + # xのヒストグラム
+  geom_line(data = true_df, aes(x = x, y = P_x), 
+            linetype = "dashed", color = "red") + # 真の確率密度
+  labs(title = "Metropolis Method", 
+       subtitle = paste0("K=", K))
+
+# 更新推移をグラフ化
+ggplot(update_df, aes(x = k, y = x)) + 
+  geom_line() + 
+  labs(title = "Metropolis Method")
+
+
+# 期待値を計算
+expected_df <- tibble(
+  k = 1:K, 
+  x = trace_x
+) %>% 
+  mutate(E_x = cumsum(x) / k) %>% 
+  mutate(E_x2 = cumsum(x^2) / k)
+
+# 期待値の推移をプロット
+ggplot(expected_df, aes(x = k)) + 
+  geom_line(aes(y = E_x), color = "purple") + # xの期待値
+  geom_hline(aes(yintercept = 0), linetype = "dashed", color = "purple") + # 真のxの期待値
+  geom_line(aes(y = E_x2), color = "blue") + # x^2の期待値
+  geom_hline(aes(yintercept = 1), linetype = "dashed", color = "blue") + # 真のx^2の期待値
+  labs(title = "Metropolis Method", 
+       y = "E[f(x)]")
+
 
 # 3.3 非周期性 --------------------------------------------------------------
+
+### 連続値をとるランダムウォーク
 
 # 繰り返し回数を指定
 K <- 1000
@@ -125,11 +164,11 @@ res_df <- tibble() # 初期化
 for(s in 1:n_simu) {
   # ランダムウォーク
   tmp_df <- tibble(
-    x = runif(n = K, min = -c, max = c), 
     k = 1:K, # 繰り返し番号
+    x = runif(n = K, min = -c, max = c), # ステップ幅を生成
+    y = cumsum(x), # k番目までの合計
     simulation = as.factor(s) # シミュレーション番号
-  ) %>% 
-    dplyr::mutate(y = cumsum(x)) # k番目までの合計
+  )
   
   # 結果を結合
   res_df <- rbind(res_df, tmp_df)
@@ -138,11 +177,20 @@ for(s in 1:n_simu) {
   print(paste0("simuration: ", s, ", y = ", tmp_df[["y"]][K]))
 }
 
+# 補助線のデータフレームを作成
+addline_df <- tibble(
+  k = 1:K, 
+  root_k = c * sqrt(k)
+)
+
 # 出力の推移をプロット
-ggplot(res_df, aes(x = k, y = y, color = simulation)) + 
-  geom_line(alpha = 0.5) + # yの推移
+ggplot() + 
+  geom_line(data = res_df, aes(x = k, y = y, color = simulation), 
+            alpha = 0.5) + # yの推移
   geom_hline(aes(yintercept = 0), linetype = "dashed") + # 期待値
+  geom_line(data = addline_df, aes(x = k, y = root_k), linetype = "dashed") + 
+  geom_line(data = addline_df, aes(x = k, y = -root_k), linetype = "dashed") + 
   theme(legend.position = "none") + # 凡例
   labs(title = "Random Walk", 
-       subtitle = paste0("p=", p, ", K=", k, "simulation:", n_simu))
+       subtitle = paste0("c=", c, ", K=", K, ", simulation:", n_simu))
 
