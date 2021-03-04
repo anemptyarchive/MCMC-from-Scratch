4be313a01d4488a061da9f3eb8e5feab60bcb84b
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2021-03-05 01:26:49 +0900

    upload ch7.2.2

--- R_Code/ch7_2.R
+++ R_Code/ch7_2.R
@@ -51,11 +51,30 @@ fn_E <- function(spin_mat, J = 1, h = 0) {
   return(energy)
 }
 
+# エネルギーの計算関数を作成:式(7.45)
+fn_E <- function(spin_mat, J = 1, h = 0) {
+  # 1辺のスピン数を取得
+  N <- nrow(spin_mat)
+  
+  # 全てのスピンの和
+  sum_s_i <- sum(spin_mat)
+  
+  # 右隣のスピンとの積の和
+  sum_s_ij_x <- sum(spin_mat[, -N] * spin_mat[, -1])
+  
+  # 下隣のスピンとの積の和
+  sum_s_ij_y <- sum(spin_mat[-1, ] * spin_mat[-N, ])
+
+  # エネルギーを計算:式(7.45)
+  energy <- - J * (sum_s_ij_x + sum_s_ij_y) - h * sum_s_i
+  return(energy)
+}
+
 
 ### 初期値の設定 -----
 
 # 1辺のスピン数を指定
-N <- 25
+N <- 100
 
 # スピンのマトリクスを初期化
 spin_mat <- matrix(sample(x = c(-1, 1), size = N^2, replace = TRUE), nrow = N, ncol = N)
@@ -84,8 +103,11 @@ J <- 1
 h <- 0
 temperature <- 1.5
 
+# 試行回数を指定
+max_iter <- 200
+
 # メトロポリス法
-for(iter in 1:100) {
+for(iter in 1:max_iter) {
   # 更新するスピン番号を生成
   n_vec <- 1:N^2 # 順番に選択
   #n_vec <- sample(1:N^2, size = N^2, replace = FALSE) # 順番をランダムに選択
@@ -139,7 +161,7 @@ for(iter in 1:100) {
   spin_df <- rbind(spin_df, tmp_spin_df)
   
   # 途中経過を表示
-  print(paste0("iter:", iter, ", accept:", n_accept))
+  print(paste0("iter:", iter, ", accept:", n_accept, ", rate:", sum(spin_mat) / N^2))
 }
 
 # 最終結果を作図
@@ -171,6 +193,161 @@ gganimate::animate(graph, nframes = iter + 1, fps = 10)
 
 
 # 7.2.2 ギブスサンプリング ---------------------------------------------------------
+
+# 7.2.2項で利用するパッケージ
+library(tidyverse)
+library(ggaimate)
+
+
+### 関数定義 -----
+
+# エネルギーの計算関数を作成:式(7.45)
+fn_E <- function(spin_mat, J = 1, h = 0) {
+  # 1辺のスピン数を取得
+  N <- nrow(spin_mat)
+  
+  # 全てのスピンの和
+  sum_s_i <- sum(spin_mat)
+  
+  # 右隣のスピンとの積の和
+  sum_s_ij_x <- sum(spin_mat[, -N] * spin_mat[, -1])
+  
+  # 下隣のスピンとの積の和
+  sum_s_ij_y <- sum(spin_mat[-1, ] * spin_mat[-N, ])
+  
+  # エネルギーを計算:式(7.45)
+  energy <- - J * (sum_s_ij_x + sum_s_ij_y) - h * sum_s_i
+  return(energy)
+}
+
+
+### 初期値の設定 -----
+
+# 1辺のスピン数を指定
+N <- 100
+
+# スピンのマトリクスを初期化
+spin_mat <- matrix(sample(x = c(-1, 1), size = N^2, replace = TRUE), nrow = N, ncol = N)
+
+# 作図用にスピンのデータフレームを作成
+spin_df <- tibble(
+  i_x = rep(1:N, times = N), 
+  i_y = rep(1:N, each = N), 
+  spin = as.vector(spin_mat), 
+  label = as.factor(paste0("iter:", 0, ", rate:", sum(spin_mat) / N^2))
+)
+
+# 初期値を作図
+ggplot(spin_df, aes(x = i_x, y = i_y, fill = spin)) + 
+  geom_tile() + # ヒートマップ
+  scale_fill_gradient(low = "red", high = "yellow", 
+                      breaks = c(-1, 1), guide = guide_legend()) + # タイルの色
+  coord_fixed(ratio = 1) + # アスペクト比
+  labs(subtitle = paste0("rate:", sum(spin_mat) / N^2))
+
+
+### ギブスサンプリングによる更新 -----
+
+# パラメータを指定
+J <- 1
+h <- 0
+temperature <- 1.5
+
+# 試行回数を指定
+max_iter <- 150
+
+# ギブスサンプリング
+for(iter in 1:max_iter) {
+  # 更新するスピン番号を生成
+  n_vec <- 1:N^2 # 順番に選択
+  #n_vec <- sample(1:N^2, size = N^2, replace = FALSE) # 順番をランダムに選択
+  #n_vec <- sample(1:N^2, size = N^2, replace = TRUE) # ランダムに選択
+  
+  # 配位を更新
+  for(n in n_vec) {
+    # スピンのインデックスを計算
+    i_y <- ifelse(n %% N == 0, yes = N, no = n %% N) # 行番号
+    i_x <- (n - 1) %/% N + 1 # 列番号
+    
+    # i番目のスピンを反転させたマトリクスを作成
+    spin_plus_mat <- spin_mat
+    spin_plus_mat[i_y, i_x] <- 1
+    spin_minus_mat <- spin_mat
+    spin_minus_mat[i_y, i_x] <- -1
+    
+    # エネルギーを計算:式(7.45)
+    energy_plus  <- - fn_E(spin_plus_mat, J, h) / temperature
+    energy_minus <- - fn_E(spin_minus_mat, J, h) / temperature
+    
+    # エネルギーの指数を計算
+    if(max(energy_plus, energy_minus) > 700) {
+      # 最大値を取得
+      max_energy <- max(energy_plus, energy_minus)
+      
+      # オーバーフロー対策
+      exp_energy_plus  <- exp(energy_plus - max_energy)
+      exp_energy_minus <- exp(energy_minus - max_energy)
+    } else if(min(energy_plus, energy_minus) < - 700) {
+      # 最小値を取得
+      min_energy <- min(energy_plus, energy_minus)
+      
+      # アンダーフロー対策
+      exp_energy_plus  <- exp(energy_plus + min_energy)
+      exp_energy_minus <- exp(energy_minus + min_energy)
+    } else {
+      exp_energy_plus  <- exp(energy_plus)
+      exp_energy_minus <- exp(energy_minus)
+    }
+    
+    # エネルギーを確率値に変換
+    p_plus <- exp_energy_plus / (exp_energy_plus + exp_energy_minus)
+    p_minus <- exp_energy_minus / (exp_energy_plus + exp_energy_minus)
+    
+    # 確率に従いスピンを更新
+    spin_mat[i_y, i_x] <- sample(c(-1, 1), size = 1, prob = c(p_minus, p_plus))
+  }
+  
+  # スピンのデータフレームを作成
+  tmp_spin_df <- tibble(
+    i_x = rep(1:N, times = N), 
+    i_y = rep(1:N, each = N), 
+    spin = as.vector(spin_mat), 
+    label = as.factor(paste0("iter:", iter, ", rate:", sum(spin_mat) / N^2))
+  )
+  
+  # 結果を結合
+  spin_df <- rbind(spin_df, tmp_spin_df)
+  
+  # 途中経過を表示
+  print(paste0("iter:", iter, ", rate:", sum(spin_mat) / N^2))
+}
+
+# 最終結果を作図
+spin_df %>% 
+  filter(label == as.character(tmp_spin_df[["label"]][1])) %>% # 最後の試行を抽出
+  ggplot(aes(x = i_x, y = i_y, fill = spin)) + 
+  geom_tile() + # ヒートマップ
+  scale_fill_gradient(low = "red", high = "yellow", 
+                      breaks = c(-1, 1), guide = guide_legend()) + # タイルの色
+  coord_fixed(ratio = 1) + # アスペクト比
+  labs(title = "Gibbs Sampling", 
+       subtitle = paste0("iter:", iter, ", rate:", sum(spin_mat) / N^2))
+
+
+### アニメーションの作成 -----
+
+# アニメーション用のグラフを作成
+graph <- ggplot(spin_df, aes(x = i_x, y = i_y, fill = spin)) + 
+  geom_tile() + # ヒートマップ
+  scale_fill_gradient(low = "red", high = "yellow", 
+                      breaks = c(-1, 1), guide = guide_legend()) + # タイルの色
+  coord_fixed(ratio = 1) + # アスペクト比
+  gganimate::transition_manual(label) + # フレーム
+  labs(title = "Gibbs Sampling", 
+       subtitle = "{current_frame}")
+
+# gif画像を出力
+gganimate::animate(graph, nframes = iter + 1, fps = 10)
 
 
 # 7.2.5 Wolffのアルゴリズム ------------------------------------------------------------
